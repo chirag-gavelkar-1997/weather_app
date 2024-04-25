a51dd0168de34c667d3e9d2d465fdfd59d2f2a73
chirag-gavelkar-1997 <163122618+chirag-gavelkar-1997@users.noreply.github.com>
2024-04-25 13:04:43 +0530

    Update SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -24,7 +24,7 @@ function SearchBar({ onSearch ,data1 }) {
         <h3 className="sm:text-xl text-xl mt-5 font-medium title-font text-gray-900">{cityName},IN</h3>
         )}
 
-        <div className="text-xs mt-5 border flex items-center justify-center  p-5 w-96  bg-sky-100 tracking-widest font-medium title-font mb-1"> 
+        <div className="text-xs mt-5 border flex items-center justify-center  p-5 sm:w-94 w-96  bg-sky-100 tracking-widest font-medium title-font mb-1"> 
         <form onSubmit={handleSubmit}>
         <input  type="text" id="name"  onChange={handleChange}  placeholder="Enter city name" name="name" className=" bg-gray-100 bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
      
